84fedebec3ecb90d8f22dd8b1c213ff6fd2f22f9
Leweke <jonathan.leweke@caci.com>
2019-04-11 13:59:41 -0400

    Content updates

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -18,7 +18,15 @@ contactContent = [{
  "feature": [{
     "highlightTitle":"",
     "highlightSubtitle":"866.618.5988",
-    "highlightText":"",
+    "highlightText": [{
+      "paragraph":"For questions or concerns with the information on your contract or the current status of an existing payment request, please contact your Contracting Officer."
+      },
+      {
+      "paragraph":"The Procurement Integrated Enterprise Environment Helpdesk CANNOT take any action on your WAWF document."
+      },
+      {
+      "paragraph":"For payment status, please validate the invoice in question is in a processed status in the WAWF application. If your document is in a processed status, please contact DFAS for payment information or go the myInvoice application, which is now a part of Procurement Integrated Enterprise Environment, or contact DFAS for payment information."
+    }],
     "highlightLinkText":"",
     "highlightLink":"tel:866.618.5988",
     "highlightLinkAccessibilityLabel":""
